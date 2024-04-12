252cf584bcc2c43e42e87df2492be0fd2ec43855
bytrustu <bytrustu@gmail.com>
2024-04-13 02:32:55 +0900

    chore: barrels config 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
-import { enableMocking } from '@/mocks';
-import './styles/index.css';
+import { enableMocking } from '@/mocks/enableMocking';
 
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
